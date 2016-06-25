134dcba3969ffeee95579507e6faa6acad21ae61
monikaciecka <ciecka256@gmail.com>
2016-06-25 09:25:23 -0400

    starting to refactor into functions, now prints for all products on page, changed tabs to spaces

--- WebsiteScrapers/UltaScraper.java
+++ WebsiteScrapers/UltaScraper.java
@@ -6,24 +6,48 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 class UltaScraper{
-	public static void main(String [] args) throws IOException{
-		Document website = Jsoup.connect("http://www.ulta.com/skin-care-suncare-sunscreen?N=27ff").get();
-		// doc.select("p.prod-desc");
-		Elements sunscreenLinks = website.select("p[class=prod-desc]");
 
-		Element oneProduct = sunscreenLinks.get(0).select("a").first();
-		String linkToProduct = "http://www.ulta.com" + oneProduct.attr("href");
-		String productName = oneProduct.text();
+    private ArrayList<String> getAllProductLinks(String pageLink) throws IOException{
 
-		Document productPage = Jsoup.connect(linkToProduct).get();
+        ArrayList<String> allProductLinks = new ArrayList<>();
 
-		Elements ingredients = productPage.select("div[id=product-default-ingredients]");
+        Document website = Jsoup.connect(pageLink).get();
+        Elements sunscreenLinks = website.select("p[class=prod-desc]");
+        for(Element product : sunscreenLinks){
+            product = product.select("a").first();
+            String linkToProduct = "http://www.ulta.com" + product.attr("href");
+            allProductLinks.add(linkToProduct);
+        }
+        return allProductLinks;
+    }
 
-		Element oneProductIngredients = ingredients.get(0).select("div").first();
+    private Map<String, ArrayList<String> > getAllProductInfo(String originLink) throws IOException{
 
-		String allIngredients = oneProductIngredients.text();
+        Map<String, ArrayList<String> > productInfo = new HashMap<>();
+        String productName = ""; 
+        ArrayList<String> allLinks = getAllProductLinks(originLink);
+        ArrayList<String> supplementInfo = new ArrayList<>();
 
-		System.out.println(productName);
-		System.out.println(allIngredients);
-	}
+        for(String link : allLinks){
+            supplementInfo.clear();
+            Document productPage = Jsoup.connect(link).get();
+            Element ingredients = productPage.select("div[id=product-default-ingredients]").get(0).select("div").first();
+            String allIngredients = ingredients.text();
+
+            Element htmlTitle = productPage.select("meta[property=og:title]").first();
+            productName = htmlTitle.attr("content");
+            System.out.println(productName);
+            System.out.println(allIngredients);
+
+            productInfo.put(productName, supplementInfo);
+        }
+        return productInfo;
+    }
+
+    public static void main(String [] args) throws IOException{
+
+        UltaScraper testScrape = new UltaScraper();
+
+        testScrape.getAllProductInfo("http://ulta.com/skin-care-suncare-sunscreen?N=27ff");
+    }
 }
