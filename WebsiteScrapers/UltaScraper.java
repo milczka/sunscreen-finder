7cb50f38c01da53cfed36f10a8269bc3c25d2c9b
monikaciecka <ciecka256@gmail.com>
2016-06-25 11:02:47 -0400

    Reduced code repetition with new functions getIngredients, GetProductName. Added a sunscreen type identifier.

--- WebsiteScrapers/UltaScraper.java
+++ WebsiteScrapers/UltaScraper.java
@@ -21,26 +21,77 @@ class UltaScraper{
         return allProductLinks;
     }
 
+    private String getIngredients(Document webpage){
+
+        Element ingredients = webpage.select("div[id=product-default-ingredients]").get(0).select("div").first();
+        String allIngredients = ingredients.text();
+
+        return allIngredients;
+    }
+
+    private String getProductName(Document webpage){
+
+        Element htmlTitle = webpage.select("meta[property=og:title]").first();
+        String productName = htmlTitle.attr("content");
+
+        return productName;
+    }
+
+    private String determineSSType(String ingredients){
+        // returns: physical, chemical, or combo
+
+        String ssType = "unknown";
+        String ingredientsLower = ingredients.toLowerCase();
+        boolean chemical = false, physical = false;
+
+        String[] chemicalIngredients = {"avobenzone", "oxybenzone", "homosalate", "octinoxate", "octocrylene", "octisalate", "ecamsule"};
+        String[] physicalIngredients = {"zinc oxide", "titanium dioxide"};
+
+        for(String ingredient : chemicalIngredients){
+            if(ingredientsLower.contains(ingredient)) chemical = true;
+        }
+
+        for(String ingredient : physicalIngredients){
+            if(ingredientsLower.contains(ingredient)) physical = true;
+        }
+
+        if(physical && chemical) ssType = "combination";
+        else if(physical) ssType = "physical";
+        else if(chemical) ssType = "chemical";
+
+
+        return ssType;
+    }
+
     private Map<String, ArrayList<String> > getAllProductInfo(String originLink) throws IOException{
 
         Map<String, ArrayList<String> > productInfo = new HashMap<>();
-        String productName = ""; 
         ArrayList<String> allLinks = getAllProductLinks(originLink);
         ArrayList<String> supplementInfo = new ArrayList<>();
 
         for(String link : allLinks){
             supplementInfo.clear();
             Document productPage = Jsoup.connect(link).get();
-            Element ingredients = productPage.select("div[id=product-default-ingredients]").get(0).select("div").first();
-            String allIngredients = ingredients.text();
-
-            Element htmlTitle = productPage.select("meta[property=og:title]").first();
-            productName = htmlTitle.attr("content");
+            String allIngredients = getIngredients(productPage);
+            String productName = getProductName(productPage);
             System.out.println(productName);
             System.out.println(allIngredients);
 
             productInfo.put(productName, supplementInfo);
         }
+        return productInfo;
+    }
+
+    private Map<String, ArrayList<String> > getOneProductInfo(String originLink) throws IOException{
+
+        Map<String, ArrayList<String> > productInfo = new HashMap<>();
+        ArrayList<String> supplementInfo = new ArrayList<>();
+
+        Document productPage = Jsoup.connect(originLink).get();
+        String allIngredients = getIngredients(productPage);
+        String productName = getProductName(productPage);
+        productInfo.put(productName, supplementInfo);
+
         return productInfo;
     }
 
